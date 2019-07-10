5415a427ea25d8670c9a174afadc6724080a2fcb
T0astBread <tb@t0ast.cc>
2019-07-10 07:32:15 +0200

    Implement upgrader role

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,9 +1,23 @@
+const { SPAWN_NAME } = require("const")
+
 module.exports = {
     /**
      * @param {Creep} creep
      **/
     run: creep => {
-        // TODO: Implement upgrader
         creep.say("Upgrading")
+        
+        if(creep.carry.energy > 0) {
+            const controller = creep.room.controller
+            if(creep.upgradeController(controller) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(controller)
+            }
+        }
+        else {
+            const spawn = Game.spawns[SPAWN_NAME]
+            if(creep.withdraw(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(spawn)
+            }
+        }
     }
 }
