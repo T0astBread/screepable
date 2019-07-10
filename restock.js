ce1d84c5be323a3076cda33eafda873e42b97a9d
T0astBread <tb@t0ast.cc>
2019-07-10 09:29:04 +0200

    Change restocking condition

--- restock.js
+++ restock.js
@@ -13,7 +13,7 @@ const {
 module.exports = () => {
     const spawn = Game.spawns[SPAWN_NAME]
     const creepCount = Object.keys(Game.creeps).length
-    if(creepCount < MAX_CREEP_COUNT && spawn.energy > ENERGY_RESERVE + CREEP_COST) {
+    if(creepCount < MAX_CREEP_COUNT && spawn.energy >= ENERGY_RESERVE + CREEP_COST) {
         const creepName = "GP_" + creepCount
         if(spawn.spawnCreep(GENERAL_PURPOSE_CREEP_CAPABILITIES, creepName) !== ERR_NOT_ENOUGH_ENERGY)
             console.log("Spawned new general purpose creep " + creepName)
