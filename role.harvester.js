ee885974a6b62e81de159b10982ca54ea27a244c
T0astBread <tb@t0ast.cc>
2019-07-10 06:36:47 +0200

    Implement harvester role

--- role.harvester.js
+++ role.harvester.js
@@ -1,9 +1,23 @@
+const { SPAWN_NAME } = require("const")
+
 module.exports = {
     /**
      * @param {Creep} creep
      **/
     run: creep => {
-        // TODO: Implement harvester
         creep.say("Harvesting")
+        const spawn = Game.spawns[SPAWN_NAME]
+        
+        if(creep.carry.energy < creep.carryCapacity) {
+            const sources = creep.room.find(FIND_SOURCES)
+            if(sources.length === 0) return
+            const firstSource = sources[0]
+            
+            if(creep.harvest(firstSource) === ERR_NOT_IN_RANGE)
+                creep.moveTo(firstSource)
+        }
+        else if(creep.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(spawn)
+        }
     }
 }
