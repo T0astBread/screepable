a7dcf9a58483c730e2ab3bfac206b589b8232841
T0astBread <tb@t0ast.cc>
2019-07-10 10:38:48 +0200

    Randomly choose & remember source as harvester

--- role.harvester.js
+++ role.harvester.js
@@ -7,14 +7,27 @@ module.exports = {
     run: creep => {
         // creep.say("Harvesting")  // Commented out because it was annoying to have almost every creep say "Harvesting"
         const spawn = Game.spawns[SPAWN_NAME]
+
+        
+        const chooseRandomSource = () => {
+            const sources = creep.room.find(FIND_SOURCES)
+            creep.memory.harvestSource = sources[Math.floor(Math.random() * sources.length)].id
+        }
+
+        if(creep.memory.harvestSource == null)
+            chooseRandomSource()
+
+        const targetSource = Game.getObjectById(creep.memory.harvestSource)
+        if(targetSource == null)
+            chooseRandomSource()
+
+        if(targetSource.energy === 0)
+            chooseRandomSource()
         
         if(creep.carry.energy < creep.carryCapacity) {
-            const sources = creep.room.find(FIND_SOURCES)
-            if(sources.length === 0) return
-            const firstSource = sources[0]
-            
-            if(creep.harvest(firstSource) === ERR_NOT_IN_RANGE)
-                creep.moveTo(firstSource)
+            if(creep.harvest(targetSource) === ERR_NOT_IN_RANGE)
+                if(creep.moveTo(targetSource) === ERR_NO_PATH)
+                    chooseRandomSource()
         }
         else if(creep.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
             creep.moveTo(spawn)
