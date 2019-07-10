4857c583699cbad4a628c5abcd810a988ebcf665
T0astBread <tb@t0ast.cc>
2019-07-10 07:43:45 +0200

    Implement builder role

--- role.builder.js
+++ role.builder.js
@@ -1,9 +1,23 @@
+const { SPAWN_NAME } = require("const")
+
 module.exports = {
     /**
      * @param {Creep} creep
      **/
     run: creep => {
-        // TODO: Implement builder
         creep.say("Building")
+        
+        if(creep.carry.energy > 0) {
+            const targetSite = creep.room.find(FIND_MY_CONSTRUCTION_SITES)[0]
+            if(creep.build(targetSite) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(targetSite)
+            }
+        }
+        else {
+            const spawn = Game.spawns[SPAWN_NAME]
+            if(creep.withdraw(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(spawn)
+            }
+        }
     }
 }
